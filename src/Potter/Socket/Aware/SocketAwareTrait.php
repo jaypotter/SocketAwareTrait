95f4c9fb4708d5551d0384767ea282e9e46a8c96
Jay Potter <j@ypotter.ca>
2024-03-30 13:22:08 -0400

    bind and connect

--- src/Potter/Socket/Aware/SocketAwareTrait.php
+++ src/Potter/Socket/Aware/SocketAwareTrait.php
@@ -8,6 +8,16 @@ use \Psr\Container\ContainerInterface, \Socket;
 
 trait SocketAwareTrait 
 {
+    final public function bindSocket(string $address, int $port = null): void
+    {
+        socket_bind($this->getSocket(), $address, $port ?? 0);
+    }
+    
+    final public function connectSocket(string $address, int $port = null): void
+    {
+        socket_connect($this->getSocket(), $address, $port);
+    }
+    
     final public function getSocket(): Socket
     {
         return $this->getContainer()->get('socket');
